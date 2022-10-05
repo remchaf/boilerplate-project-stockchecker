73ff106450db3a1b4e201f1c7267782c938c8b79
remchaf <70279203+remchaf@users.noreply.github.com>
2022-10-05 13:46:34 +0000

    fif !

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,152 @@
-'use strict';
+"use strict";
+require("dotenv").config();
+var mongoose = require("mongoose");
+var { Schema } = mongoose;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// 1- Defining my stockSchema
+const stockSchema = new Schema({
+  stock: {
+    type: String,
+    required: true,
+  },
+  IP: [String],
+  // likes: {
+  //   type: Number,
+  //   default: 0,
+  // },
+});
+
+const Stock = mongoose.model("Stock", stockSchema);
 
 module.exports = function (app) {
+  const url = [
+    "https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/",
+    "/quote",
+  ];
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+  app.route("/api/stock-prices").get(function (req, res) {
+    const ip = req.socket.remoteAddress;
+
+    if (params.length == 2) {
+      const [stock, likey] = req.params;
+      let stockData = 0;
+
+      fetch(params[0])
+        .then((response) => response.json())
+        .then((data) => {
+          stockData = {
+            stock: data.symbol,
+            price: data.close,
+          };
+        })
+        .catch((err) => {
+          res.json({
+            stockData: { error: "External data ressource !", likes: 0 },
+          });
+        });
+
+      const query = Stock.find({ stock: stock });
+
+      query.then((err, data) => {
+        if (err) {
+          Stock.create({ stock, IP: [ip], likes: likey ? 1 : 0 });
+
+          stockData.likes = likey ? 1 : 0;
+          res.json({ stockData });
+          return;
+        }
+
+        IPs = data.IP;
+
+        if (IPs.some((d) => d == ip)) {
+          stockData.likes = IPs.length;
+          res.json({ stockData });
+          return;
+        } else {
+          IPs.push(ip);
+          data.save((err) => {
+            if (err) {
+              stockData.likes = IPs.length;
+              res.send({ stockData });
+              return console.log("Unable to like !");
+            }
+
+            stockData.likes = IPs.length + 1;
+            res.json({ stockData });
+            return console.log("Done !");
+          });
+        }
+      });
+    }
+
+    // Case of two stocks
+    else {
+      const stocks = params.slice(2),
+        like = params[-1];
+      const fetchs = Promise.all(stocks);
+      fetchs
+        .then((response) => response.json())
+        .then((dataArr) => {
+          // const [stock1, stock2] = dataArr;
+          const [stockData1, stockData2] = [
+            { stock: dataArr[0].stock, price: dataArr[0].close},
+            { stock: dataArr[1], price: dataArr[1].close},
+          ];
+
+          // First query
+          const query1 = Stock.find({ stock: stocks[0] });
+          let likes1;
+
+          query1.then((err, data) => {
+            if (err) {
+              Stock.create({ stock: stocks[0], IP: [ip] });
+              likes1 = like ? 1 : 0;
+            } else {
+              if (data.IP.some((d) => d == ip)) {
+                likes1 = data.IP.length;
+              } else {
+                data.IP.push(ip);
+                data.save();
+                likes1 = data.IP.length + 1;
+              }
+            }
+          });
+
+          // Second query
+          const query2 = Stock.find({ stock: stocks[1] });
+          let likes2 = 0;
+
+          query2.then((err, data) => {
+            if (err) {
+              Stock.create({ stock: stocks[1], IP: [ip] });
+              likes2 = like ? 1 : 0;
+            } else {
+              if (data.IP.some((d) => d == ip)) {
+                likes2 = data.IP.length;
+              } else {
+                data.IP.push(ip);
+                data.save((err, data) => {
+                  if(err) return console.log("Unable to save to the database !")
+                  likes2 = data.IP.length + 1;
+                });
+              }
+            }
+          });
+
+          // Defining the rel_likes
+          rel = likes1 - likes2
+          stockData1.rel_likes = rel >= 0 ? rel : -rel
+          stockData2.rel_likes = rel < 0 ? rel : -rel
+
+          res.json({
+            stockData: [stockData1, stockData2]
+          })
+        });
+    }
+  });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,71 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function () {
+  test("#1 - Viewing one stack: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices/stock=goog&like=false")
+      .end((err, res) => {
+        assert.status(200);
+        assert.property(
+          res.body,
+          "stockData",
+          "stockData should be a property of the return object !"
+        );
+        assert.property(res.body.stockData, "stock");
+        assert.property(res.body.stockData, "price");
+        assert.property(res.body.stockData, "likes");
+      });
+    done();
+  });
 
+  test("#2 - Viewing one stock and liking it: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices/stock=msft&like=true")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.likes, 1);
+      });
+    done();
+  });
+
+  test("#3 - Viewing the same stock and liking it again: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices/stock=msft&like=true")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.stockData.like, 1);
+      });
+    done();
+  });
+
+  test("#4 - Viewing two stocks: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices/stock=aap&stock=googl&like=false")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.typeOf(res.body.stockData, "array");
+        assert.property(res.body[0], "rel_likes");
+      });
+    done();
+  });
+
+  test("#5 - Viewing two stocks and liking them: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices/stock=goog&stock=msft&like=true")
+      .end(function (err, res) {
+        assert.equal(res.body.stockData[0].rel_likes, 1);
+        assert.equal(res.body.stockData.rel_likes, -1);
+      });
+    done();
+  });
 });
